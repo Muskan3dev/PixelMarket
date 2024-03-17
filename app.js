ac86665d132e22460ede79d3532bf60fffa97acb
Muskan3dev <muskannovember3@gmail.com>
2024-03-17 23:33:33 +0530

    Added express.static() method to render static files

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
