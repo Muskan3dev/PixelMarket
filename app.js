6013b610e6eb2b039b5e1b3a7f1ee477eb90a740
Muskan3dev <muskannovember3@gmail.com>
2024-03-17 13:17:57 +0530

    Added handling routes in middleware

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ const express = require("express");
 const app = express();
 
 //middleware
-app.use((req, res, next) => {
-  console.log("In the middleware");
+app.use("/", (req, res, next) => {
+  console.log("This always runs");
   next();
 });
 
-app.use((req, res, next) => {
+app.use("/add-product", (req, res, next) => {
   console.log("In another middleware");
-  res.send("<h1>Hello from Express js</h1>");
+  res.send("<h1>The Add product page</h1>");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("Greetings");
+  res.send("<h1>Hello from express</h1>");
 });
 
 app.listen(3000);
