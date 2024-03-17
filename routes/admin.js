91d7699222a64e20e6cc13732007fc07ec56b425
Muskan3dev <muskannovember3@gmail.com>
2024-03-17 20:07:58 +0530

    Added res.sendFile() method to render html file

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,14 @@
+const path = require("path");
+
 const express = require("express");
 
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/admin/product" method="POST" ><input type="text" name="product name"><button type="Submit">Add product</button></form>'
-  );
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
-router.post("/product", (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
