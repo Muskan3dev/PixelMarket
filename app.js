8e83a60c686126ca5bf293119ad972b8dff787db
Muskan3dev <muskannovember3@gmail.com>
2024-03-19 19:25:52 +0530

    Added  error controller and connect with existing route

--- app.js
+++ app.js
@@ -3,17 +3,19 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
 
-const adminData = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const errorController = require("./controllers/error");
+
+app.set("view engine", "ejs");
+app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminData.routes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-});
+app.use(errorController.get404);
 
-app.listen(3000);
+app.listen(5000);
