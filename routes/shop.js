f2f7279c29b6c642a5153ae57f9fc2b51f64ae5a
Muskan3dev <muskannovember3@gmail.com>
2024-03-19 19:23:43 +0530

    import product controller to use with existing routes

--- routes/shop.js
+++ routes/shop.js
@@ -2,13 +2,10 @@ const path = require("path");
 
 const express = require("express");
 
+const productController = require("../controllers/products");
+
 const router = express.Router();
 
-const adminData = require("./admin");
-
-router.get("/", (req, res, next) => {
-  console.log("shop.js", adminData.products);
-  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
-});
+router.get("/", productController.getProducts);
 
 module.exports = router;
