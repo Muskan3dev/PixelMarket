ea5f1e5f61cdc9a75f82b09f5691996c07ba1f5e
Muskan3dev <muskannovember3@gmail.com>
2024-03-20 15:37:01 +0530

    Added controllers for shop routes

---
+++ controllers/shop.js
@@ -0,0 +1,37 @@
+const Product = require("../models/product");
+
+exports.getProducts = (req, res, next) => {
+  const products = Product.fetchAll((products) => {
+    res.render("shop/product-list", {
+      prods: products,
+      pageTitle: "All Products",
+      path: "/products",
+    });
+  });
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("shop/index", {
+      prods: products,
+      pageTitle: "Shop",
+      path: "/",
+    });
+  });
+};
+
+exports.getCart = (req, res, next) => {
+  res.render("shop/cart", {
+    prods: Product,
+    path: "/cart",
+    pageTitle: "Your Cart",
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render("shop/checkout", {
+    prods: Product,
+    path: "/checkout",
+    pageTitle: "checkout",
+  });
+};
