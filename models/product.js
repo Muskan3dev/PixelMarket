43006afd360b106135cf7c2b72a10b96db1a2991
Muskan3dev <muskannovember3@gmail.com>
2024-03-24 18:53:45 +0530

    Added deleteById method

--- models/product.js
+++ models/product.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const cart = require("./cart");
 const p = path.join(
   path.dirname(require.main.filename),
   "data",
@@ -40,6 +41,18 @@ module.exports = class Product {
           console.log(err);
         });
       }
+    });
+  }
+
+  static deleteById(id) {
+    getProductsFromFile((products) => {
+      const product = products.find((prod) => prod.id === id);
+      const updatedProducts = products.filter((prod) => prod.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+        if (!err) {
+          cart.deleteProduct(id, product.price);
+        }
+      });
     });
   }
 
