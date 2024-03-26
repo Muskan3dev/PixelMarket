b8d9912b73cf3e3d0cea89d562d7b49cbca93950
Muskan3dev <muskannovember3@gmail.com>
2024-03-26 18:39:14 +0530

    Added product model with save() method

--- models/product.js
+++ models/product.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+/* const fs = require("fs");
 const path = require("path");
 const cart = require("./cart");
 const p = path.join(
@@ -68,3 +68,27 @@ module.exports = class Product {
     });
   }
 };
+ */
+const getDb = require("../util/database").getDb;
+class Product {
+  constructor(title, price, description, imageUrl) {
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
+  }
+  save() {
+    const db = getDb();
+    return db
+      .collection("products")
+      .insertOne(this)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
+
+module.exports = Product;
