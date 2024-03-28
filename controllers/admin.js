868b4bdcdeba484a1f1fd19d0d77389286096255
Muskan3dev <muskannovember3@gmail.com>
2024-03-28 13:25:52 +0530

    Fix prodId value to string and modified postDeleteProduct method

--- controllers/admin.js
+++ controllers/admin.js
@@ -87,7 +87,7 @@ exports.getProducts = (req, res, next) => {
 };
 
 exports.postDeleteProduct = (req, res, next) => {
-  const prodId = +req.body.productId;
+  const prodId = req.body.productId;
   Product.deleteById(prodId)
     .then(() => {
       console.log("DESTROYED PRODUCT");
