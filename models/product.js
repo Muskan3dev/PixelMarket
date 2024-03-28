e42350dbed452ce58991ec2094c0662871380ef8
Muskan3dev <muskannovember3@gmail.com>
2024-03-28 12:05:07 +0530

    Added id in the product model constructor and modified save() method to update product

--- models/product.js
+++ models/product.js
@@ -69,20 +69,31 @@ module.exports = class Product {
   }
 };
  */
+
 const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
+
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = id;
   }
+
   save() {
     const db = getDb();
-    return db
-      .collection("products")
-      .insertOne(this)
+    let dbOp;
+    if (this._id) {
+      // Update the product
+      dbOp = db
+        .collection("products")
+        .updateOne({ _id: this._id }, { $set: this });
+    } else {
+      dbOp = db.collection("products").insertOne(this);
+    }
+    return dbOp
       .then((result) => {
         console.log(result);
       })
@@ -90,6 +101,7 @@ class Product {
         console.log(err);
       });
   }
+
   static fetchAll() {
     const db = getDb();
     return db
@@ -104,6 +116,7 @@ class Product {
         console.log(err);
       });
   }
+
   static findById(prodId) {
     const db = getDb();
     return db
@@ -113,6 +126,18 @@ class Product {
       .then((product) => {
         console.log(product);
         return product;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  static deleteById(prodId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then((result) => {
+        console.log("Deleted");
       })
       .catch((err) => {
         console.log(err);
