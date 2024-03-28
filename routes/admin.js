c1bcaf13b08aa99aa4f65eee515bf47bea7cbe17
Muskan3dev <muskannovember3@gmail.com>
2024-03-28 12:06:26 +0530

    Added router for edit product

--- routes/admin.js
+++ routes/admin.js
@@ -2,14 +2,14 @@ const path = require("path");
 
 const express = require("express");
 
-const router = express.Router();
-
 const adminController = require("../controllers/admin");
 
-// /admin/add-product => GET
-router.get("/add-product", adminController.getAddroduct);
+const router = express.Router();
 
-// /admin/product => GET
+// /admin/add-product => GET
+router.get("/add-product", adminController.getAddProduct);
+
+// /admin/products => GET
 router.get("/products", adminController.getProducts);
 
 // /admin/add-product => POST
