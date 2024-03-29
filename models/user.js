cddb36a710df511db38f53da9ad0a201cf872cb0
Muskan3dev <muskannovember3@gmail.com>
2024-03-29 18:17:44 +0530

    Added addToCart() method in user model and Fix cart error by initializing this.cart as an object with items array property

--- models/user.js
+++ models/user.js
@@ -4,14 +4,48 @@ const getDb = require("../util/database").getDb;
 const ObjectId = mongodb.ObjectId;
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
+    this.cart = cart;
+    this._id = id;
   }
 
   save() {
     const db = getDb();
     return db.collection("users").insertOne(this);
+  }
+
+  addToCart(product) {
+    // Initialize this.cart as an object with items array property
+    if (!this.cart) {
+      this.cart = { items: [] };
+    }
+    const cartProductIndex = this.cart.items.findIndex((cp) => {
+      // console.log("Product id", cp.productId);
+      return cp.productId.toString() === product._id.toString();
+    });
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      updatedCartItems.push({
+        productId: new ObjectId(product._id),
+        quantity: 1,
+      });
+    }
+    const updatedCart = {
+      items: updatedCartItems,
+    };
+    const db = getDb();
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
   }
 
   static findById(userId) {
@@ -28,4 +62,5 @@ class User {
       });
   }
 }
+
 module.exports = User;
