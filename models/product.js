2857fadd54a0d69185db98d931c160acea01e643
Muskan3dev <muskannovember3@gmail.com>
2024-03-30 17:46:47 +0530

    Added product schema

--- models/product.js
+++ models/product.js
@@ -1,76 +1,28 @@
-/* const fs = require("fs");
-const path = require("path");
-const cart = require("./cart");
-const p = path.join(
-  path.dirname(require.main.filename),
-  "data",
-  "product.json"
-);
-const getProductsFromFile = (cb) => {
-  fs.readFile(p, (err, fileContent) => {
-    if (err) {
-      cb([]);
-    } else {
-      cb(JSON.parse(fileContent));
-    }
-  });
-};
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
-  save() {
-    getProductsFromFile((products) => {
-      if (this.id) {
-        const existingProductIndex = products.findIndex(
-          (prod) => prod.id === this.id
-        );
-        const updatedProducts = [...products];
-        updatedProducts[existingProductIndex] = this;
-        fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
-          console.log(err);
-        });
-      } else {
-        this.id = Math.random();
-        products.push(this);
-        fs.writeFile(p, JSON.stringify(products), (err) => {
-          console.log(err);
-        });
-      }
-    });
-  }
+const mongoose = require("mongoose");
 
-  static deleteById(id) {
-    getProductsFromFile((products) => {
-      const product = products.find((prod) => prod.id === id);
-      const updatedProducts = products.filter((prod) => prod.id !== id);
-      fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
-        if (!err) {
-          cart.deleteProduct(id, product.price);
-        }
-      });
-    });
-  }
+const Schema = mongoose.Schema;
 
-  static fetchAll(cb) {
-    getProductsFromFile(cb);
-  }
+const productSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+});
 
-  static findById(id, cb) {
-    getProductsFromFile((products) => {
-      // console.log({ products, id });
-      const product = products.find((p) => p.id === id);
-      cb(product);
-    });
-  }
-};
- */
-
-const mongodb = require("mongodb");
+module.exports = mongoose.model("Product", productSchema);
+/* const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
 
 class Product {
@@ -147,3 +99,4 @@ class Product {
 }
 
 module.exports = Product;
+ */
