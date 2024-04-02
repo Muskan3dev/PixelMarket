95378cd7e89e7ce50d857f4d366ca7ece0090dc4
Muskan3dev <muskannovember3@gmail.com>
2024-04-02 23:59:37 +0530

    Added postLogin middleware for storing user's data in session and fixing error by using JSON.stringify() method to store session data of user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,11 @@
+const User = require("../models/user");
+
 exports.getLogin = (req, res, next) => {
   //   const isLoggedIn = req
   //     .get('Cookie')
   //     .split(';')[1]
   //     .trim()
   //     .split('=')[1] === 'true';
-  console.log(req.session.isLoggedIn);
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
@@ -13,8 +14,21 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  req.session.isLoggedIn = true;
-  res.redirect("/");
+  User.findById("6608226ee36f07a34efcdae1")
+    .then((user) => {
+      console.log({ user });
+      req.session.isLoggedIn = true;
+      req.session.user = JSON.stringify(user);
+      // req.session.user = user;
+      console.log({ session: req.session });
+      req.session.save((err) => {
+        console.log(err);
+        res.redirect("/");
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 exports.postLogout = (req, res, next) => {
