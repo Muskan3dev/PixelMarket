4622db56856d1c9c4446b88e7d5f64da88bc9716
Muskan3dev <muskannovember3@gmail.com>
2024-04-03 12:28:48 +0530

    Added bcrypt package ,getSignup middleware and postSignup middleware with an authentication flow

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,18 @@
 const User = require("../models/user");
 
+const bcrypt = require("bcryptjs");
+
 exports.getLogin = (req, res, next) => {
-  //   const isLoggedIn = req
-  //     .get('Cookie')
-  //     .split(';')[1]
-  //     .trim()
-  //     .split('=')[1] === 'true';
   res.render("auth/login", {
     path: "/login",
+    pageTitle: "Login",
+    isAuthenticated: false,
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render("auth/signup", {
+    path: "/signup",
     pageTitle: "Login",
     isAuthenticated: false,
   });
@@ -25,6 +30,33 @@ exports.postLogin = (req, res, next) => {
         console.log(err);
         res.redirect("/");
       });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  User.findOne({ email: email })
+    .then((userDoc) => {
+      if (userDoc) {
+        return res.redirect("/signup");
+      }
+      return bcrypt.hash(password, 12);
+    })
+    .then((hashedPassword) => {
+      const user = new User({
+        email: email,
+        password: hashedPassword,
+        cart: { items: [] },
+      });
+      return user.save();
+    })
+    .then((result) => {
+      res.redirect("/login");
     })
     .catch((err) => {
       console.log(err);
