1f9ce4469f84230323f50b946947d9333c49afbe
Muskan3dev <muskannovember3@gmail.com>
2024-04-06 23:46:33 +0530

    Imported csurf  register in middleware and pass isAuthenticated and csrfToken property to all rendered views

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
 const flash = require("connect-flash");
+const csrf = require("csurf");
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
@@ -18,6 +19,7 @@ const store = new MongoDBStore({
   uri: MONGODB_URI,
   collection: "sessions",
 });
+const csrfProtection = csrf();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -36,6 +38,7 @@ app.use(
     store: store,
   })
 );
+app.use(csrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
@@ -48,6 +51,12 @@ app.use((req, res, next) => {
       next();
     })
     .catch((err) => console.log(err));
+});
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
 });
 
 app.use("/admin", adminRoutes);
