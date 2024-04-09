afbe26be195e0048efd15e35129e695104a9d41b
Muskan3dev <muskannovember3@gmail.com>
2024-04-09 11:16:43 +0530

    Added validation in postAddProduct and postEditProduct middleware

--- controllers/admin.js
+++ controllers/admin.js
@@ -9,6 +9,7 @@ exports.getAddProduct = (req, res, next) => {
     editing: false,
     hasError: false,
     errorMessage: null,
+    validationErrors: [],
   });
 };
 
@@ -35,6 +36,7 @@ exports.postAddProduct = (req, res, next) => {
         description: description,
       },
       errorMessage: errors.array()[0].msg,
+      validationErrors: errors.array(),
     });
   }
   const product = new Product({
@@ -73,6 +75,8 @@ exports.getEditProduct = (req, res, next) => {
         editing: editMode,
         product: product,
         hasError: false,
+        errorMessage: null,
+        validationErrors: [],
       });
     })
     .catch((err) => console.log(err));
@@ -86,6 +90,25 @@ exports.postEditProduct = (req, res, next) => {
   const updatedDesc = req.body.description;
   const sessionData = JSON.parse(req.session.user);
   const userId = sessionData._id;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: true,
+      hasError: true,
+      product: {
+        title: updatedTitle,
+        imageUrl: updatedImageUrl,
+        price: updatedPrice,
+        description: updatedDesc,
+        _id: prodId,
+      },
+      errorMessage: errors.array()[0].msg,
+      validationErrors: errors.array(),
+    });
+  }
   Product.findById(prodId)
     .then((product) => {
       if (product.userId.toString() !== userId.toString()) {
