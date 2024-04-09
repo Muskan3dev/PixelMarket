ff676f2caa0033177076b00e42ac0600ecef8791
Muskan3dev <muskannovember3@gmail.com>
2024-04-09 11:15:06 +0530

    Added validation in post edit-product route

--- routes/admin.js
+++ routes/admin.js
@@ -29,7 +29,17 @@ router.post(
 
 router.get("/edit-product/:productId", isAuth, adminController.getEditProduct);
 
-router.post("/edit-product", isAuth, adminController.postEditProduct);
+router.post(
+  "/edit-product",
+  [
+    body("title").isString().isLength({ min: 3 }).trim(),
+    body("imageUrl").isURL(),
+    body("price").isNumeric(),
+    body("description").isLength({ min: 5, max: 500 }).trim(),
+  ],
+  isAuth,
+  adminController.postEditProduct
+);
 
 router.post("/delete-product", isAuth, adminController.postDeleteProduct);
 
