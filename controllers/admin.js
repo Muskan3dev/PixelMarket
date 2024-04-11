b2131342766b9cf687d45f15f6092a0dc2b63071
Muskan3dev <muskannovember3@gmail.com>
2024-04-11 13:30:34 +0530

    Modified postAdd product and getEdit product middleware  for storing file data in database

--- controllers/admin.js
+++ controllers/admin.js
@@ -15,30 +15,47 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
-  const imageUrl = req.body.imageUrl;
+  const image = req.file;
   const price = req.body.price;
   const description = req.body.description;
   const sessionData = JSON.parse(req.session.user);
   const userId = sessionData._id;
-  console.log({ reqd: req.body, r: req.session.user });
+
+  if (!image) {
+    return res.status(422).render("admin/edit-product", {
+      pageTitle: "Add Product",
+      path: "/admin/add-product",
+      editing: false,
+      hasError: true,
+      product: {
+        title: title,
+        price: price,
+        description: description,
+      },
+      errorMessage: "Attached file is not an image",
+      validationErrors: [],
+    });
+  }
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     console.log(errors.array());
     return res.status(422).render("admin/edit-product", {
       pageTitle: "Add Product",
-      path: "/admin/edit-product",
+      path: "/admin/add-product",
       editing: false,
       hasError: true,
       product: {
         title: title,
-        imageUrl: imageUrl,
         price: price,
+        image: image,
         description: description,
       },
       errorMessage: errors.array()[0].msg,
       validationErrors: errors.array(),
     });
   }
+  const imageUrl = image.path;
+  console.log("imageurl", imageUrl);
   const product = new Product({
     title: title,
     price: price,
@@ -49,6 +66,7 @@ exports.postAddProduct = (req, res, next) => {
   product
     .save()
     .then((result) => {
+      console.log({ result: result });
       console.log("Created Product");
       res.redirect("/admin/products");
     })
@@ -91,7 +109,7 @@ exports.postEditProduct = (req, res, next) => {
   const prodId = req.body.productId;
   const updatedTitle = req.body.title;
   const updatedPrice = +req.body.price;
-  const updatedImageUrl = req.body.imageUrl;
+  const image = req.file;
   const updatedDesc = req.body.description;
   const sessionData = JSON.parse(req.session.user);
   const userId = sessionData._id;
@@ -105,7 +123,6 @@ exports.postEditProduct = (req, res, next) => {
       hasError: true,
       product: {
         title: updatedTitle,
-        imageUrl: updatedImageUrl,
         price: updatedPrice,
         description: updatedDesc,
         _id: prodId,
@@ -122,7 +139,9 @@ exports.postEditProduct = (req, res, next) => {
       product.title = updatedTitle;
       product.price = updatedPrice;
       product.description = updatedDesc;
-      product.imageUrl = updatedImageUrl;
+      if (image) {
+        product.imageUrl = image.path;
+      }
       return product.save().then((result) => {
         console.log("UPDATED PRODUCT!");
         res.redirect("/admin/products");
